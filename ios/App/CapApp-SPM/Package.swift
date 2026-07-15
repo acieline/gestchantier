3f509ae77165484ad3d01b2e7b7b83f89e5c5a59
acieline <spotbeenthomas@gmail.com>
2026-07-16 00:52:07 +0200

    Update Package.swift

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "7.6.8"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", .upToNextMajor(from: "7.0.0")),
         .package(name: "CapgoCapacitorNativeBiometric", path: "../../../node_modules/@capgo/capacitor-native-biometric")
     ],
     targets: [
